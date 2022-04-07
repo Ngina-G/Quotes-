3af515ab591c3b9d5ff48daf86879d10ff45ebf8
Ngina-G <Nginagitau11@outlook.com>
2022-04-07 12:55:17 +0300

    updates: adds goal and banner components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quotes } from './quotes';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
-}
+  quotes:Quote[]= [
+    {id:1, name:"Wine and children speak the truth."},
+    {id:2, name:"How you make your bed is how you are going to sleep."},
+    {id:3, name:"Wait for the wisest of all counselors, time."},
+    {id:4, name:"Wellbeing is attained by little and little, and nevertheless is no little thing itself."},
+    {id:5, name:"Madness does not go to the mountains, it goes to people."},
+  ];
+};
